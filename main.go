ff9c07d8fa9f9f4146657fd8ca288a3fd7317950
hogashi <azuma.ginkakuji@gmail.com>
2022-03-05 15:47:19 +0900

    replace line-head space-ish to space

--- main.go
+++ main.go
@@ -4,12 +4,21 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strings"
+	"unicode/utf8"
 )
 
 func main() {
+	re := regexp.MustCompile(`^[ 　\t]+`)
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		result := re.ReplaceAllStringFunc(scanner.Text(), func(s string) string {
+			spaceCount := utf8.RuneCountInString(s)
+			return strings.Repeat(" ", spaceCount)
+		})
+		fmt.Println(result)
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
