dfb7844a3c1ac5d4eebf0ef5cc32bcec8eef18a6
hogashi <azuma.ginkakuji@gmail.com>
2022-03-14 05:41:46 +0900

    add replace with option

--- main.go
+++ main.go
@@ -7,16 +7,26 @@ import (
 	"regexp"
 	"strings"
 	"unicode/utf8"
+
+	"github.com/jessevdk/go-flags"
 )
 
 func main() {
+	var opts struct {
+		ReplaceWith string `short:"w" long:"with" description:"replace with this string"`
+	}
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		panic(err)
+	}
+
 	re := regexp.MustCompile(`^[ 　\t]+`)
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		result := re.ReplaceAllStringFunc(scanner.Text(), func(s string) string {
 			spaceCount := utf8.RuneCountInString(s)
-			return strings.Repeat(" ", spaceCount)
+			return strings.Repeat(opts.ReplaceWith, spaceCount)
 		})
 		fmt.Println(result)
 	}
