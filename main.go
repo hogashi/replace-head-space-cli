3fe6d30c3bd2af3967aeeab1668d5af85b3a1672
hogashi <azuma.ginkakuji@gmail.com>
2022-03-14 05:52:21 +0900

    define as default value, fix replace with

--- main.go
+++ main.go
@@ -11,27 +11,35 @@ import (
 	"github.com/jessevdk/go-flags"
 )
 
+var defaultTarget = `^[ 　\t]+`
+var defaultReplaceWith = " "
+
 func main() {
 	var opts struct {
-		Target      string `short:"t" long:"target" description:"replace target RegExp"`
-		ReplaceWith string `short:"w" long:"with" description:"replace with this string"`
+		Target      string `short:"t" long:"target" description:"replace target RegExp (default: '^[ 　\t]+')"`
+		ReplaceWith string `short:"w" long:"with" description:"replace with this string (default: ' ')"`
 	}
 	_, err := flags.Parse(&opts)
 	if err != nil {
 		panic(err)
 	}
 
-	target := `^[ 　\t]+`
+	target := defaultTarget
 	if opts.Target != "" {
 		target = opts.Target
 	}
 	re := regexp.MustCompile(target)
 
+	replaceWith := defaultReplaceWith
+	if opts.ReplaceWith != "" {
+		replaceWith = opts.ReplaceWith
+	}
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		result := re.ReplaceAllStringFunc(scanner.Text(), func(s string) string {
 			spaceCount := utf8.RuneCountInString(s)
-			return strings.Repeat(opts.ReplaceWith, spaceCount)
+			return strings.Repeat(replaceWith, spaceCount)
 		})
 		fmt.Println(result)
 	}
