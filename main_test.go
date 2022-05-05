daf0d4e2821886746b87fc40d94ba514e690f43f
hogashi <azuma.ginkakuji@gmail.com>
2022-05-05 13:53:27 +0900

    add test cases for target, replaceWith

--- main_test.go
+++ main_test.go
@@ -4,24 +4,25 @@ import "testing"
 
 func TestReplaceHead(t *testing.T) {
 	cases := []struct {
-		in, want string
+		target, replaceWith, in, want string
 	}{
-		{"no space", "no space"},
-		{" 1 half", " 1 half"},
-		{"　1 full", " 1 full"},
-		{"	1 tab", " 1 tab"},
-		{"   3 half", "   3 half"},
-		{"　　　3 full", "   3 full"},
-		{"			3 tab", "   3 tab"},
-		{" 　1 half, 1 full (2)", "  1 half, 1 full (2)"},
-		{" 	1 half, 1 tab (2)", "  1 half, 1 tab (2)"},
-		{"　	1 full, 1 tab (2)", "  1 full, 1 tab (2)"},
-		{" 　	1 half, 1 full, 1 tab (3)", "   1 half, 1 full, 1 tab (3)"},
+		{`[ 　\t]+`, " ", "no space", "no space"},
+		{`[ 　\t]+`, " ", " 1 half", " 1 half"},
+		{`[ 　\t]+`, " ", "　1 full", " 1 full"},
+		{`[ 　\t]+`, " ", "	1 tab", " 1 tab"},
+		{`[ 　\t]+`, " ", " 　	1 half 1 full 1 tab (3)", "   1 half 1 full 1 tab (3)"},
+		{`[ 　\t]+`, "ABC", "no space", "no space"},
+		{`[ 　\t]+`, "ABC", " 1 half", "ABC1 half"},
+		{`[ 　\t]+`, "ABC", "　1 full", "ABC1 full"},
+		{`[ 　\t]+`, "ABC", "	1 tab", "ABC1 tab"},
+		{`[ 　\t]+`, "ABC", " 　	1 half 1 full 1 tab (3)", "ABCABCABC1 half 1 full 1 tab (3)"},
+		{`[a-z]+`, "ABC", "no space", "ABCABC space"},
+		{`[a-z]+`, "ABC", "	1 tab", "	1 tab"},
 	}
 	for _, c := range cases {
-		got := ReplaceHead(c.in, `[ 　\t]+`, " ")
+		got := ReplaceHead(c.in, c.target, c.replaceWith)
 		if got != c.want {
-			t.Errorf("ReplaceHead(%q) == %q, want %q", c.in, got, c.want)
+			t.Errorf("ReplaceHead(%q, %q, %q) == %q, want %q", c.in, c.target, c.replaceWith, got, c.want)
 		}
 	}
 }
